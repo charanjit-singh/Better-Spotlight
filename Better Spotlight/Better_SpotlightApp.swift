bb10d00a286b094fb42b84161e40851b954ede37
Charanjit Singh <charanjitdotsingh@gmail.com>
2026-08-11 00:05:10 +0530

    Initial Commit

---
+++ Better Spotlight/Better_SpotlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_SpotlightApp.swift
+//  Better Spotlight
+//
+//  Created by Charanjit Singh on 11/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Better_SpotlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Better Spotlight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Better Spotlight
+//
+//  Created by Charanjit Singh on 11/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
